2dd2cfb43f58f6307529700ac65970bdc6a14c91
Marcel Dittrich <76435@studmail.htw-aalen.de>
2023-05-07 18:55:46 +0200

    Testing of Basic setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as pd
+
+# For running of streamlit, please use the command "python -m streamlit run main.py"
+# Streamlit course
+
+st.title("**Hello World!**")
+st.subheader("How are you?")
