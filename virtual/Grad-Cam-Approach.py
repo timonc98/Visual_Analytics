a5f949fb83449a67081aba974d866e92ecdbac37
Marcel Dittrich <76435@studmail.htw-aalen.de>
2023-06-02 12:59:46 +0200

    Add Grad-Cam Approach

---
+++ virtual/Grad-Cam-Approach.py
@@ -0,0 +1,392 @@
+
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import os
+from glob import glob
+from PIL import Image
+import numpy as np
+import pandas as pd
+import cv2
+import random
+import albumentations as A
+import keras
+import matplotlib.cm as cm
+import plotly.express as px
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+import seaborn as sns
+from livelossplot.inputs.keras import PlotLossesCallback
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+from sklearn.model_selection import train_test_split
+#from keras.preprocessing import image
+from keras.utils import load_img, img_to_array, array_to_img
+from tensorflow.keras import layers, models
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report, recall_score, accuracy_score, precision_score, f1_score
+
+files = ['Normal', 'COVID', 'Lung_Opacity','Viral Pneumonia']
+path = "D:/Daten-Marcel/2.Fachsemester/01_Visual Analytics/Projekt\Visual_Analytics/virtual\Dataset"
+data_dir = os.path.join(path)
+print(path)
+
+data = []
+for id, level in enumerate(files):
+    for file in os.listdir(os.path.join(data_dir, level+'/'+'images')):
+#         data.append(['{}/{}'.format(level, file), level])
+        data.append([level +'/' +'images'+ '/'+file, level])
+        
+
+data = pd.DataFrame(data, columns = ['image_file', 'corona_result'])
+
+data['path'] = path + '/' +data['image_file']
+data['corona_result'] = data['corona_result'].map({'Normal': 'Normal', 'COVID': 'Covid_positive', 'Lung_Opacity':'Lung_Opacity', 'Viral Pneumonia':'Viral_Pneumonia'})
+
+data.head()
+
+print('Number of Duplicated Samples: %d'%(data.duplicated().sum()))
+print('Number of Total Samples: %d'%(data.isnull().value_counts()))
+
+no_of_samples = 21165
+df = pd.DataFrame()
+df['corona_result'] = ['Normal', 'Covid_positive', 'Lung_Opacity', 'Viral_Pneumonia']
+df['Count'] = [len(data[data['corona_result'] == 'Normal']), len(data[data['corona_result'] == 'Covid_positive']), len(data[data['corona_result'] == 'Lung_Opacity']), len(data[data['corona_result'] == 'Viral_Pneumonia'])]
+df = df.sort_values(by = ['Count'], ascending = False)
+
+fig = px.bar(df, x = 'corona_result', y = 'Count', 
+             color = "corona_result",  width = 600, 
+             color_continuous_scale='BrBg')
+
+fig.update_traces(textfont_size = 12, textangle = 0, textposition = "outside", cliponaxis = False)
+
+fig.show()
+
+data['image'] = data['path'].map(lambda x: np.asarray(Image.open(x).resize((75,75))))
+
+mean_val = []
+std_dev_val = []
+max_val = []
+min_val = []
+
+for i in range(0, no_of_samples):
+    mean_val.append(data['image'][i].mean())
+    std_dev_val.append(np.std(data['image'][i]))
+    max_val.append(data['image'][i].max())
+    min_val.append(data['image'][i].min())
+
+imageEDA = data.loc[:,['image','corona_result','path']]
+imageEDA['mean'] = mean_val
+imageEDA['stedev'] = std_dev_val
+imageEDA['max'] = max_val
+imageEDA['min'] = min_val
+
+imageEDA['subt_mean'] = imageEDA['mean'].mean() - imageEDA['mean']
+
+mean_normal = imageEDA["mean"].loc[(imageEDA["corona_result"] == "Normal")].to_numpy()
+mean_covid = imageEDA["mean"].loc[(imageEDA["corona_result"] == "Covid_positive")].to_numpy()
+mean_opacity = imageEDA["mean"].loc[(imageEDA["corona_result"] == "Lung_Opacity")].to_numpy()
+mean_pneumonia = imageEDA["mean"].loc[(imageEDA["corona_result"] == "Viral_Pneumonia")].to_numpy()
+mean_list = [mean_normal,mean_covid,mean_opacity,mean_pneumonia]
+
+max_normal = imageEDA["max"].loc[(imageEDA["corona_result"] == "Normal")].to_numpy()
+max_covid = imageEDA["max"].loc[(imageEDA["corona_result"] == "Covid_positive")].to_numpy()
+max_opacity = imageEDA["max"].loc[(imageEDA["corona_result"] == "Lung_Opacity")].to_numpy()
+max_pneumonia = imageEDA["max"].loc[(imageEDA["corona_result"] == "Viral_Pneumonia")].to_numpy()
+max_list = [max_normal,max_covid,max_opacity,max_pneumonia]
+
+min_normal = imageEDA["min"].loc[(imageEDA["corona_result"] == "Normal")].to_numpy()
+min_covid = imageEDA["min"].loc[(imageEDA["corona_result"] == "Covid_positive")].to_numpy()
+min_opacity = imageEDA["min"].loc[(imageEDA["corona_result"] == "Lung_Opacity")].to_numpy()
+min_pneumonia = imageEDA["min"].loc[(imageEDA["corona_result"] == "Viral_Pneumonia")].to_numpy()
+min_list = [min_normal,min_covid,min_opacity,min_pneumonia]
+
+std_normal = imageEDA["stedev"].loc[(imageEDA["corona_result"] == "Normal")].to_numpy()
+std_covid = imageEDA["stedev"].loc[(imageEDA["corona_result"] == "Covid_positive")].to_numpy()
+std_opacity = imageEDA["stedev"].loc[(imageEDA["corona_result"] == "Lung_Opacity")].to_numpy()
+std_pneumonia = imageEDA["stedev"].loc[(imageEDA["corona_result"] == "Viral_Pneumonia")].to_numpy()
+std_list = [std_normal,std_covid,std_opacity,std_pneumonia]
+
+Names = ['Normal','Covid','Lung opacity','Pneumonia']
+
+def min_max_plot(data,name,title):
+    colors = ['#04EEFF', '#4BBBFF', '#F5F232', '#7FFF00']
+
+    # Create distplot with curve_type set to 'normal'
+    fig = ff.create_distplot(data, name, show_hist=False, colors = colors)
+    fig.update_layout(title_text=title,
+                      titlefont={'size': 20},
+                     template="plotly_dark",
+                     width=800,height=600,)
+    fig.show()
+
+min_max_plot(max_list,Names,"Images Colour Max Value Distribution by Class")
+
+min_max_plot(min_list,Names,"Images Colour Min Value Distribution by Class")
+
+min_max_plot(mean_list,Names,"Images Colour Mean Value Distribution by Class")
+
+fig = px.scatter(imageEDA, x="mean", y="stedev", color='corona_result',color_discrete_sequence=px.colors.qualitative.Set3)
+fig.update_layout(title_text="Mean and Standard Deviation of Image Samples",
+                  xaxis_title='Image Channel Colour Mean',
+                  yaxis_title='Image Channel Colour Standard Deviation',
+                  titlefont={'size': 20},
+                  template="plotly_dark",
+                  width=800,height=600,)
+fig.show()
+
+n_samples = 3
+
+fig, m_axs = plt.subplots(4, n_samples, figsize = (6*n_samples, 3*4))
+
+for n_axs, (type_name, type_rows) in zip(m_axs, data.sort_values(['corona_result']).groupby('corona_result')):
+    n_axs[1].set_title(type_name, fontsize = 20)
+    for c_ax, (_, c_row) in zip(n_axs, type_rows.sample(n_samples, random_state = 1234).iterrows()):       
+        picture = c_row['path']
+        image = cv2.imread(picture)
+        c_ax.imshow(image)
+        c_ax.axis('off')
+
+
+
+
+all_data = []
+
+# Storing images and their labels into a list for further Train Test split
+{'Normal': 'Normal', 'COVID': 'Covid_positive', 'Lung_Opacity':'Lung_Opacity', 'Viral Pneumonia':'Viral_Pneumonia'}
+for i in range(len(data)):
+    image = cv2.imread(data['path'][i])
+    image = cv2.resize(image, (70, 70)) / 255.0
+    label = 0
+    if data['corona_result'][i] == "Normal":
+        label = 0
+    elif data['corona_result'][i] == "Covid_positive":
+        label = 1
+    elif data['corona_result'][i] == "Lung_Opacity":
+        label = 2
+    else:
+        label = 3
+    label = 1 if data['corona_result'][i] == "Positive" else 0
+    all_data.append([image, label])
+
+x = []
+y = []
+
+for image, label in all_data:
+    x.append(image)
+    y.append(label)
+
+# Converting to Numpy Array    
+x = np.array(x)
+y = np.array(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.1, random_state = 42)
+
+print(x_train.shape, x_test.shape, x_val.shape, y_train.shape, y_test.shape, y_val.shape)
+
+def create_model(n_classes, train_shape):
+    cnn_model = models.Sequential()
+    cnn_model.add(layers.Conv2D(filters = 128, kernel_size = (3, 3), activation = 'relu', input_shape = train_shape))
+    cnn_model.add(layers.MaxPooling2D((2, 2)))
+    cnn_model.add(layers.Dropout(0.3))
+
+    cnn_model.add(layers.Conv2D(filters = 64, kernel_size = (3, 3), activation = 'relu'))
+    cnn_model.add(layers.MaxPooling2D((2, 2)))
+    cnn_model.add(layers.Dropout(0.5))
+
+    cnn_model.add(layers.Conv2D(filters = 32, kernel_size = (3, 3), activation = 'relu'))
+    cnn_model.add(layers.Flatten())
+    cnn_model.add(layers.Dense(units = 16, activation = 'relu'))
+    cnn_model.add(layers.Dropout(0.2))
+
+    cnn_model.add(layers.Dense(units = 4))
+
+    cnn_model.compile(optimizer = 'adam', 
+               loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True), 
+               metrics = ['accuracy'])
+
+    cnn_model.summary()
+    return cnn_model
+
+input_shape = (70, 70, 3)
+n_classes= 4
+
+conv_model = create_model(n_classes, input_shape)
+
+plot_loss_1 = PlotLossesCallback()
+
+#tl_checkpoint_1 = ModelCheckpoint(filepath='tl_model_v1.weights.best.hdf5',
+#                                  save_best_only=True,
+#                                  verbose=1)
+
+
+print("Test")
+
+plot_loss_2 = PlotLossesCallback()
+
+conv_history = conv_model.fit(x_train, y_train, 
+                        epochs = 5, batch_size = 256,  
+                        validation_data = (x_val, y_val), 
+                        
+                       verbose=1)
+
+#callbacks = [tl_checkpoint_1, plot_loss_2],
+yp_train = conv_model.predict(x_train)
+yp_train = np.argmax(yp_train, axis = 1)
+
+yp_val = conv_model.predict(x_val)
+yp_val = np.argmax(yp_val, axis = 1)
+
+yp_test = conv_model.predict(x_test)
+yp_test = np.argmax(yp_test, axis = 1)
+
+def confusion_matrix_train_test_val(name, y_train, yp_train, y_val, yp_val, y_test, yp_test):
+    
+    print("\n-----------------------------{}-----------------------------\n".format(name))
+    
+    
+    print("Classification Report for Train Data\n")
+    print(classification_report(y_train, yp_train)) 
+    print("--------------------------------------------------------------------------")
+    
+    print("\nClassification Report for Validation Data\n")
+    print(classification_report(y_val, yp_val))   
+    print("--------------------------------------------------------------------------")
+
+    
+    print("\nClassification Report for Test Data\n")
+    print(classification_report(y_test, yp_test))   
+    print("--------------------------------------------------------------------------")
+
+
+# Summarize History for Accuracy
+
+plt.plot(conv_history.history['accuracy'])
+plt.plot(conv_history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc = 'lower right')
+plt.show()
+
+# Summarize History for Loss
+
+plt.plot(conv_history.history['loss'])
+plt.plot(conv_history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc = 'upper right')
+plt.show()
+
+# Accuracy Loss Graph
+
+pd.DataFrame(conv_history.history).plot()
+plt.title('Model Accuracy/Loss')
+plt.ylabel('Accuracy/Loss')
+plt.xlabel('Epoch')
+plt.show()
+
+
+model_builder = keras.applications.xception.Xception
+img_size = (299, 299)
+preprocess_input = keras.applications.xception.preprocess_input
+decode_predictions = keras.applications.xception.decode_predictions
+imag = []
+
+last_conv_layer_name = "block14_sepconv2_act"
+
+# To Get Image into numpy array
+
+def get_img_array(img_path, size):
+    img = load_img(img_path, target_size = size) 
+    array = img_to_array(img) 
+    array = np.expand_dims(array, axis = 0)
+    return array
+
+# Top create heatmaps for the samples
+
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index = None):
+    grad_model = tf.keras.models.Model([model.inputs], [model.get_layer(last_conv_layer_name).output, model.output])
+
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
+
+    # Storing Heatmap values into list
+
+covid_noncovid_heatmap = []
+
+list_images_sample = ["D:/Daten-Marcel/2.Fachsemester/01_Visual Analytics/Projekt\Visual_Analytics/virtual/Dataset/Viral Pneumonia/images/Viral Pneumonia-1003.png",
+                      "D:/Daten-Marcel/2.Fachsemester/01_Visual Analytics/Projekt\Visual_Analytics/virtual/Dataset/Normal/images/Normal-10004.png",
+                      "D:/Daten-Marcel/2.Fachsemester/01_Visual Analytics/Projekt\Visual_Analytics/virtual/Dataset/Lung_Opacity/images/Lung_Opacity-1000.png",
+                      "D:/Daten-Marcel/2.Fachsemester/01_Visual Analytics/Projekt\Visual_Analytics/virtual/Dataset/COVID/images/COVID-1008.png"]
+
+for i in list_images_sample:
+    img_array = preprocess_input(get_img_array(i, size = img_size))
+    model = model_builder(weights = "imagenet")
+    model.layers[-1].activation = None
+    preds = model.predict(img_array)
+    heatmap = make_gradcam_heatmap(img_array, model, last_conv_layer_name)
+    covid_noncovid_heatmap.append(heatmap)
+
+# To Display GradCAM output for the samples
+
+def save_and_display_gradcam(img_path, heatmap, cam_path = "cam.jpg", alpha = 0.4):
+    img = load_img(img_path)
+    img = img_to_array(img)
+
+    heatmap = np.uint8(255 * heatmap)
+
+    jet = cm.get_cmap("jet")
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+    jet_heatmap = array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = img_to_array(jet_heatmap)
+
+    superimposed_img = jet_heatmap * alpha + img
+    superimposed_img = array_to_img(superimposed_img)
+    superimposed_img.save(cam_path)
+    
+    imag.append(cv2.imread(img_path))
+    imag.append(cv2.imread("./cam.jpg"))
+
+
+
+for i in range(len(list_images_sample)):
+    save_and_display_gradcam(list_images_sample[i], covid_noncovid_heatmap[i])
+
+def plot_multiple_img(img_matrix_list, title_list, ncols, main_title = ""):
+    
+    fig, myaxes = plt.subplots(figsize = (15, 8), nrows = 2, ncols = ncols, squeeze = False)
+    fig.suptitle(main_title, fontsize = 18)
+    fig.subplots_adjust(wspace = 0.3)
+    fig.subplots_adjust(hspace = 0.3)
+    
+    for i, (img, title) in enumerate(zip(img_matrix_list, title_list)):
+        myaxes[i // ncols][i % ncols].imshow(img)
+        myaxes[i // ncols][i % ncols].set_title(title, fontsize = 15)
+        
+    plt.show()
+
+# list_images_sample = ["/kaggle/input/covid19-radiography-database/COVID-19_Radiography_Dataset/Viral Pneumonia/images/Viral Pneumonia-1003.png",
+#                      "/kaggle/input/covid19-radiography-database/COVID-19_Radiography_Dataset/Normal/images/Normal-10004.png",
+#                      "/kaggle/input/covid19-radiography-database/COVID-19_Radiography_Dataset/Lung_Opacity/images/Lung_Opacity-1000.png",
+#                      "/kaggle/input/covid19-radiography-database/COVID-19_Radiography_Dataset/COVID/images/COVID-1008.png"]
+
+titles_list = ["Viral Pneumonia",'Viral Pneumonia Grad','Normal','Normal Grad','Lung_Opacity','Lung_Opacity Grad','COVID','COVID Grad']
+
+plot_multiple_img(imag, titles_list, ncols = 4, main_title = "GRAD-CAM COVID-19 Image Analysis")
