837adc8c672ca555c3fb47c2aad9cb0afd990e49
timonc98 <timon-clauss@gmx.de>
2023-06-23 12:24:25 +0200

    Ordnerstruktur

--- virtual/Archiv/main.py
+++ virtual/Archiv/main.py
@@ -1,9 +1,30 @@
-import streamlit as st
-import pandas as pd
-import numpy as pd
+import PIL
+import streamlit
+import numpy
+import pandas
+import cv2
+import random
+import albumentations
+import plotly
+import matplotlib
+import seaborn
+import sklearn
+import keras
+import tensorflow
+import sklearn.metrics
+import matplotlib.cm
 
-# For running of streamlit, please use the command "python -m streamlit run main.py"
-# Streamlit course
+print("PIL (Python Imaging Library):", PIL.__version__)
+print("streamlit:", streamlit.__version__)
+print("numpy:", numpy.__version__)
+print("pandas:", pandas.__version__)
+print("cv2 (OpenCV):", cv2.__version__)
+print("albumentations:", albumentations.__version__)
+print("plotly:", plotly.__version__)
+print("matplotlib:", matplotlib.__version__)
+print("seaborn:", seaborn.__version__)
+print("sklearn:", sklearn.__version__)
+print("keras:", keras.__version__)
+print("tensorflow:", tensorflow.__version__)
+print("sklearn:", sklearn.__version__)
 
-st.title("**Hello World!**")
-st.subheader("How are you?")
